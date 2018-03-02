46e7313f6fd63ff03fc16567df3e94363b304434
anubhav3 <anubhav.iiser@gmail.com>
2018-03-02 23:44:55 +0530

    change in pi.py

--- pi.py
+++ pi.py
@@ -2,10 +2,11 @@ from random import random
 from math import pi
 count = 0
 sim = 1000000
-for i in range(sim):
+for i in range(sim):SEG WE Q	
 	x  = random()
 	y = random()
 	if x**2+y**2 <1:
 		count += 1
 print "Pi value generated = ", 4*count/(float(sim))
-print "True value of pi = ", pi
+print "True value of pi = ", picdsvsda es 
+eef FEW
