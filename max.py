ff7704d84cc42443ee1e0b2038c0c4f96b71bce8
anubhav3 <anubhav.iiser@gmail.com>
2018-03-02 23:43:35 +0530

    Yeah

---
+++ max.py
@@ -0,0 +1,6 @@
+a = [1,2,3,4,5,44,3,3,55,44,34,43,2]
+max = a[0]
+for i in a:
+	if max<i:
+		max = i	
+print max
---
+++ pi.py
@@ -0,0 +1,11 @@
+from random import random
+from math import pi
+count = 0
+sim = 1000000
+for i in range(sim):
+	x  = random()
+	y = random()
+	if x**2+y**2 <1:
+		count += 1
+print "Pi value generated = ", 4*count/(float(sim))
+print "True value of pi = ", pi
